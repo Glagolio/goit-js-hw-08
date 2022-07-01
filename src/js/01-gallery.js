91e15c4fb09934691a19d71acd0f9f68bcd07453
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-01 20:34:24 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const listGalleryEl = galleryItems.map(image =>
+    `<a class="gallery__item" href="${image.original}">
+            <img 
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+            />
+        </a>
+    `).join('');
+
+galleryEl.insertAdjacentHTML("beforeend", listGalleryEl);
+
+let lightBox = new SimpleLightbox('.gallery .gallery__item', { captionsData: 'alt', captionDelay: '250' });
+
